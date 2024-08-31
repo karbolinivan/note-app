8b8b55f2a1fed6f8657031cde3e0f9c65a3b881a
Karbolin Ivan <karboliniv@gmail.com>
2024-08-31 16:14:48 +0300

    button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import './App.css';
+import Button from './components/Button/Button';
 
+// используем функциональный подход
 function App() {
-
-
-
-  
   return (
-
-      <div>
-
-      </div>
-
+    // должен быть один родительский элемент <>
+    <>
+      <h1>Title</h1>
+      <p>Description</p>
+      <Button />
+    </>
   );
+  // возвращаем jsx разметку, которая потом преобразуется в js код
 }
 
 export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+function Button() {
+  return (
+    <>
+      <button>Соанить</button>
+    </>
+  );
+}
+
+export default Button;
