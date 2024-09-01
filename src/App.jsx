9890099cdbdb62f6fe630f43dec82edabbc6b00c
karbolinivan <karboliniv@gmail.com>
2024-09-01 17:39:09 +0300

    composition

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
 import './App.css';
 import Button from './components/Button/Button';
+import CardButton from './components/CardButton/CardButton';
 import JornalItem from './components/JornalItem/JornalItem';
 
 // используем функциональный подход
 function App() {
-  return (
-    // должен быть один родительский элемент <>
-    <>
-      <h1>Title</h1>
-      <p>Description</p>
-      <Button />
-      <JornalItem />
-    </>
-  );
-  // возвращаем jsx разметку, которая потом преобразуется в js код
+
+	const data = [
+		{
+			title: 'Подготовка к обновлению курсов',
+			date: new Date(),
+			text: 'Текст'
+		},
+		{
+			title: 'Поход в горы',
+			date: new Date(),
+			text: 'Текст'
+		}
+	];
+	return (
+	// должен быть один родительский элемент <>
+		<>
+			<h1>Title</h1>
+			<p>Description</p>
+			<Button />
+			<CardButton>
+				<JornalItem 
+					title={data[0].title}
+					data={data[0].date}
+					text={data[0].text}
+				/>
+			</CardButton>
+			<CardButton>
+				<JornalItem 
+					title={data[1].title}
+					data={data[1].date}
+					text={data[1].text}
+				/>
+			</CardButton>
+		</>
+	);
+	// возвращаем jsx разметку, которая потом преобразуется в js код
 }
 
 export default App;
--- src/components/JornalItem/JornalItem.jsx
+++ src/components/JornalItem/JornalItem.jsx
@@ -1,20 +1,17 @@
 import './JornalItem.css';
 
-function JornalItem() {
-  const title = 'Подготовка к обновлению курсов';
-  const date = new Date();
-  const text = 'Текст';
-  return (
-    <>
-      <div className="jornal-item">
-        <h2 className="jornal-item__header">{title}</h2>
-        <h2 className="jornal-item__body">
-          <div className="jornal-item__date">{date.toString()}</div>
-          <div className="jornal-item__text">{text}</div>
-        </h2>
-      </div>
-    </>
-  );
+function JornalItem({ title, date, text }) {
+	const formatDate = new Intl.DateTimeFormat('ru-RU').format(date);
+
+	return (
+		<>
+			<h2 className="jornal-item__header">{title}</h2>
+			<h2 className="jornal-item__body">
+				<div className="jornal-item__date">{formatDate}</div>
+				<div className="jornal-item__text">{text}</div>
+			</h2>
+		</>
+	);
 }
 
 export default JornalItem;
