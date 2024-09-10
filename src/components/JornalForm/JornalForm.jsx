e4eb3af76e4d6dc45affe1074485c2edacc4f0c5
Karbolin Ivan <karboliniv@gmail.com>
2024-09-10 09:34:42 +0300

    refactor

--- src/components/JornalForm/JornalForm.jsx
+++ src/components/JornalForm/JornalForm.jsx
@@ -4,69 +4,98 @@ import { useEffect, useReducer } from 'react';
 import cn from 'classnames';
 import { formReducer, INITIAL_STATE } from './JornalForm.state';
 
-
 function JornalForm({ onSubmit }) {
-	const [formState, dispatchForm ] = useReducer(formReducer, INITIAL_STATE);
-	const { isValid, isFormReadyToSubmit, values } = formState;
+  const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE);
+  const { isValid, isFormReadyToSubmit, values } = formState;
 
-	useEffect(() => {
-		let timerId;
-		if (!isValid.date || !isValid.post || !isValid.title){
-			timerId = setTimeout(() => {
-				dispatchForm({ type: 'RESET_VALIDITY' });
-			}, 2000);
-		}
-		return () => {
-			clearTimeout(timerId);
-		};
-	}, [ isValid ]);
+  useEffect(() => {
+    let timerId;
+    if (!isValid.date || !isValid.post || !isValid.title) {
+      timerId = setTimeout(() => {
+        dispatchForm({ type: 'RESET_VALIDITY' });
+      }, 2000);
+    }
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [isValid]);
 
-	useEffect(() => {
-		if (isFormReadyToSubmit) {
-			onSubmit(values);
-		}
-	}, [isFormReadyToSubmit]);
+  useEffect(() => {
+    if (isFormReadyToSubmit) {
+      onSubmit(values);
+      dispatchForm({ type: 'CLEAR' });
+    }
+  }, [isFormReadyToSubmit]);
 
-	const addJornalItem = (e) => {
-		e.preventDefault();
-		const formData = new FormData(e.target);
-		const formProps = Object.fromEntries(formData);
-		dispatchForm({ type: 'SUBMIT', payload: formProps });
-	};
+  const onChange = (e) => {
+    dispatchForm({
+      type: 'SET_VALUE',
+      payload: { [e.target.name]: e.target.value },
+    });
+  };
 
-	return (
-		<>
-			<form className={styles['jornal-form']} onSubmit={addJornalItem}>
-				<div>
-					<input type='text' name='title' className={cn(styles['input-title'], {
-						[styles['invalid']]: !formState.isValid.title
-					})}/>
-				</div>
-				<div className={styles['form-row']}>
-					<label htmlFor='date' className={styles['form-label']}>
-						<img src='/calendar.svg' alt='Календарь'></img>
-						<span>Дата</span>
-					</label>
-					<input type='date' id='date' name='date' className={cn(styles['input'], {
-						[styles['invalid']]: !formState.isValid.date
-					})}/>
-				</div>
+  const addJornalItem = (e) => {
+    e.preventDefault();
+    dispatchForm({ type: 'SUBMIT' });
+  };
 
-				<div className={styles['form-row']}>
-					<label htmlFor='ta' className={styles['form-label']}>
-						<img src='/tag.svg' alt='Метка'></img>
-						<span>Метки</span>
-					</label>
-					<input type='text' id='tag' name='tag' className={cn(styles['input'])}/>			
-				</div>
-			
-				<textarea name="post" id="" className={cn(styles['input'], {
-					[styles['invalid']]: !formState.isValid.post
-				})} ></textarea>
-				<Button text='Сохранить' />
-			</form>
-		</>
-	);
+  return (
+    <>
+      <form className={styles['jornal-form']} onSubmit={addJornalItem}>
+        <div>
+          <input
+            type="text"
+            onChange={onChange}
+            value={values.title}
+            name="title"
+            className={cn(styles['input-title'], {
+              [styles['invalid']]: !formState.isValid.title,
+            })}
+          />
+        </div>
+        <div className={styles['form-row']}>
+          <label htmlFor="date" className={styles['form-label']}>
+            <img src="/calendar.svg" alt="Календарь"></img>
+            <span>Дата</span>
+          </label>
+          <input
+            type="date"
+            onChange={onChange}
+            value={values.date}
+            id="date"
+            name="date"
+            className={cn(styles['input'], {
+              [styles['invalid']]: !formState.isValid.date,
+            })}
+          />
+        </div>
+        <div className={styles['form-row']}>
+          <label htmlFor="tag" className={styles['form-label']}>
+            <img src="/tag.svg" alt="Метка"></img>
+            <span>Метки</span>
+          </label>
+          <input
+            type="text"
+            onChange={onChange}
+            value={values.tag}
+            id="tag"
+            name="tag"
+            className={cn(styles['input'])}
+          />
+        </div>
+        <textarea
+          onChange={onChange}
+          value={values.post}
+          id=""
+          name="post"
+          className={cn(styles['input'], {
+            [styles['invalid']]: !formState.isValid.post,
+          })}
+        ></textarea>
+        <Button text="Сохранить" />
+      </form>
+    </>
+  );
 }
 
 export default JornalForm;
--- src/components/JornalForm/JornalForm.state.js
+++ src/components/JornalForm/JornalForm.state.js
@@ -1,34 +1,39 @@
 export const INITIAL_STATE = {
-	isValid: {
-		post: true,
-		title: true,
-		date: true
-	},
-	values: {
-		post: undefined,
-		title: undefined,
-		date: undefined
-	},
-	isFormReadyToSubmit: false
+  isValid: {
+    post: true,
+    title: true,
+    date: true,
+  },
+  values: {
+    post: '',
+    title: '',
+    date: '',
+    tag: '',
+  },
+  isFormReadyToSubmit: false,
 };
 
 export function formReducer(state, action) {
-	switch(action.type) {
-	case 'RESET_VALIDITY':
-		return { ...state, isValid: INITIAL_STATE.isValid };
-	case 'SUBMIT': {
-		const titleValidity =  action.payload.title?.trim().length;
-		const postValidity =  action.payload.post?.trim().length;
-		const dateValidity =  action.payload.date?.trim().length;
-		return {
-			values: action.payload,
-			isValid: {
-				post: postValidity,
-				title: titleValidity,
-				date: dateValidity
-			},
-			isFormReadyToSubmit: postValidity && titleValidity && dateValidity
-		};
-	}
-	}
+  switch (action.type) {
+    case 'SET_VALUE':
+      return { ...state, values: { ...state.values, ...action.payload } };
+    case 'CLEAR':
+      return { ...state, values: INITIAL_STATE.values };
+    case 'RESET_VALIDITY':
+      return { ...state, isValid: INITIAL_STATE.isValid };
+    case 'SUBMIT': {
+      const titleValidity = state.values.title?.trim().length;
+      const postValidity = state.values.post?.trim().length;
+      const dateValidity = state.values.date;
+      return {
+        ...state,
+        isValid: {
+          post: postValidity,
+          title: titleValidity,
+          date: dateValidity,
+        },
+        isFormReadyToSubmit: postValidity && titleValidity && dateValidity,
+      };
+    }
+  }
 }
